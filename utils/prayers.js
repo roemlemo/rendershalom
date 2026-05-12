9ca4bc6c0c8c0ad91578d13013bf495c1a1f32c4
Román Ledesma <ledesmaroman@outlook.com>
2026-05-12 17:02:20 -0600

    [UPDATE] Add more prayers

--- utils/prayers.js
+++ utils/prayers.js
@@ -15,7 +15,7 @@ export const prayers = {
         {
             id: "osmaca-001", 
             title: "Oración a San Martín Caballero",
-            description: "Encomienda tus negocios para que nunca te falte salud, trabajo y sustento.",
+            description: "Encomienda tus negocios para que nunca te falte salud, trabajo y sustento",
             imageUrl: "https://imgs.search.brave.com/IDLZjI2fe6B5renHkQ5aO5l-32lw6UcV88eA5SzejU8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9oYWJs/ZW1vc2RlcmVsaWdp/b24uY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDE3LzEyL3Nh/bi1tYXJ0aW4tY2Fi/YWxsZXJvLTExLmpw/Zw"
         },
         {
@@ -29,6 +29,19 @@ export const prayers = {
             title: "Credo de los Apóstoles",
             description: "Declara tu intención de seguir a Jesucristo como Señor y Salvador",
             imageUrl: "https://imgs.search.brave.com/iHqWH_LNFxMOGYcNnRUnPWAId48k4drG-pBN6lqwZzI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNTM4/MTMzNTEyL2VzL2Zv/dG8vc3RhaW5lZC1n/bGFzcy1pbi1zYWlu/dC1ob25vciVDMyVB/OS1kZXlsYXUtY2h1/cmNoLXRoZS1sYXN0/LXN1cHBlci5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9UFhj/ZnlpZGo0alNMMWZy/Zkl5dFpwTnA4V05Y/b3k3TEFPQnBEMlht/VDlTaz0"
-        }
+        },
+        {
+            id: "ogcrni-001", 
+            title: "Credo Niceno",
+            description: "Declaración de fe cristiana más antigua y ampliamente aceptada",
+            imageUrl: "https://imgs.search.brave.com/rpb1Jc0T9_fMcRCb_sVqrxELY0AkMpSEKL4o3AvV3B0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi8zLzMxL05p/Y2FlYV9pY29uLmpw/Zy85NjBweC1OaWNh/ZWFfaWNvbi5qcGc"
+        },
+        {
+            id: "ogmag-001", 
+            title: "Magnificat",
+            description: "Alabanza pronunciada por la Virgen María durante su visita a su prima Isabel",
+            imageUrl: "https://imgs.search.brave.com/_HJzad_zMEuZg_JtW_IcqsX41QD09EihM0UbooGK1Qk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi9hL2E4L0Rv/bWVuaWNvX2doaXJs/YW5kYWlvJTJDX3Zp/c2l0YXppb25lJTJD/XzE0OTElMkNfZGFf/Y2FwcGVsbGFfZGlf/bG9yZW56b190b3Ju/YWJ1b25pX2luX3Mu/bS5fbWFkZGFsZW5h/X2RlJTI3X3Bhenpp/X2FfZmklMkNfMDIu/SlBHLzEyODBweC1E/b21lbmljb19naGly/bGFuZGFpbyUyQ192/aXNpdGF6aW9uZSUy/Q18xNDkxJTJDX2Rh/X2NhcHBlbGxhX2Rp/X2xvcmVuem9fdG9y/bmFidW9uaV9pbl9z/Lm0uX21hZGRhbGVu/YV9kZSUyN19wYXp6/aV9hX2ZpJTJDXzAy/LkpQRw"
+        },
+
     ]
 };
--- utils/prayersContent.js
+++ utils/prayersContent.js
@@ -43,5 +43,23 @@ Amén.`,
     
 Creo en el Espíritu Santo, la santa Iglesia católica, la Comunión de los Santos, el perdón de los pecados, la resurrección de la carne y la vida eterna.
     
+Amén.`,
+
+    "ogcrni-001": `Creo en un solo Dios, Padre todopoderoso, Creador del cielo y de la tierra, de todo lo visible y lo invisible. Creo en un solo Señor, Jesucristo, Hijo único de Dios, nacido del Padre antes de todos los siglos: Dios de Dios, Luz de Luz, Dios verdadero de Dios verdadero, engendrado, no creado, de la misma naturaleza del Padre, por quien todo fue hecho; que por nosotros, los hombres, y por nuestra salvación bajó del cielo, y por obra del Espíritu Santo se encarnó de María, la Virgen, y se hizo hombre; y por nuestra causa fue crucificado en tiempos de Poncio Pilato; padeció y fue sepultado, y resucitó al tercer día, según las Escrituras, y subió al cielo, y está sentado a la derecha del Padre; y de nuevo vendrá con gloria para juzgar a vivos y muertos, y su reino no tendrá fin.
+
+Creo en el Espíritu Santo, Señor y dador de vida, que procede del Padre y del Hijo, que con el Padre y el Hijo recibe una misma adoración y gloria, y que habló por los profetas.
+
+Creo en la Iglesia, que es una, santa, católica y apostólica. Confieso que hay un solo Bautismo para el perdón de los pecados. Espero la resurrección de los muertos y la vida del mundo futuro.
+
+Amén.`,
+
+    "ogmag-001": `Proclama mi alma la grandeza del Señor,y se alegra mi espíritu en Dios, mi Salvador; porque ha mirado la humillación de su esclava.
+
+Desde ahora me felicitarán todas las generaciones, porque el Poderoso ha hecho obras grandes en mí: su nombre es santo, y su misericordia llega a sus fieles de generación en generación.
+
+Él hace proezas con su brazo: dispersa a los soberbios de corazón, derriba del trono a los poderosos y enaltece a los humildes, a los hambrientos los colma de bienes y a los ricos los despide vacíos.
+
+Auxilia a Israel, su siervo, acordándose de la misericordia como lo había prometido a nuestros padres en favor de Abrahán y su descendencia por siempre.
+
 Amén.`
 };
