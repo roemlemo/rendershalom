fba9e515e08a73d7c10181a3212fcf9cff3fcec7
Román Ledesma <ledesmaroman@outlook.com>
2026-06-07 01:36:44 -0600

    [UPDATE] New images for prayers

--- utils/prayers.js
+++ utils/prayers.js
@@ -4,49 +4,49 @@ export const prayers = {
             id: "ovima-001", 
             title: "Oración a la Virgen María",
             description: "Encomienda tu día y tus intenciones a nuestra Madre",
-            imageUrl: "https://imgs.search.brave.com/5osM33q_YL3I6ZdFNu8YQ4CneyvxOf2EwN8ct_e1-uc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kZXNk/ZWxhZmUubXgvd3At/Y29udGVudC91cGxv/YWRzLzIwMjAvMTIv/dmlyZ2VuLWRlLWd1/YWRhbHVwZS5qcGVn"
+            imageUrl: "https://res.cloudinary.com/dujwpfuml/image/upload/v1780817461/virgen-guadalupe_rsee8q.png"
         },
         {
             id: "osjo-001", 
             title: "Oración a San José Obrero",
             description: "Pide una oportunidad laboral digna, estable y justa",
-            imageUrl: "https://imgs.search.brave.com/5Jr8sySUsfdUeFKOUIwZpctj_kvCHKazPOwTmWNkuJA/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/Y2xhcmluLmNvbS9p/bWcvMjAyMy8wNC8x/Mi9YT2dOaW0wS01f/NzIweDBfXzEuanBn"
+            imageUrl: "https://res.cloudinary.com/dujwpfuml/image/upload/v1780816962/san-jose-obrero_ipg1um.png"
         },
         {
             id: "osmaca-001", 
             title: "Oración a San Martín Caballero",
             description: "Encomienda tus negocios para que nunca te falte salud, trabajo y sustento",
-            imageUrl: "https://imgs.search.brave.com/IDLZjI2fe6B5renHkQ5aO5l-32lw6UcV88eA5SzejU8/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9oYWJs/ZW1vc2RlcmVsaWdp/b24uY29tL3dwLWNv/bnRlbnQvdXBsb2Fk/cy8yMDE3LzEyL3Nh/bi1tYXJ0aW4tY2Fi/YWxsZXJvLTExLmpw/Zw"
+            imageUrl: "https://res.cloudinary.com/dujwpfuml/image/upload/v1780703632/san-martin-caballero_qc1zvk.png"
         },
         {
             id: "ogacco-001", 
             title: "Acto de Contrición",
             description: "Reconcíliate y proponte evitar el pecado",
-            imageUrl: "https://imgs.search.brave.com/uD_mVcP3_uCy8TxmLiJvmPg63UytSXfuhnHYN7hRtBk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tLm1l/ZGlhLWFtYXpvbi5j/b20vaW1hZ2VzL0kv/NDFPUFFMeG9uK0wu/anBn"
+            imageUrl: "https://res.cloudinary.com/dujwpfuml/image/upload/v1780816962/acto-de-contricion_w8soq8.png"
         },
         {
             id: "ogcrap-001", 
             title: "Credo de los Apóstoles",
             description: "Declara tu intención de seguir a Jesucristo como Señor y Salvador",
-            imageUrl: "https://imgs.search.brave.com/iHqWH_LNFxMOGYcNnRUnPWAId48k4drG-pBN6lqwZzI/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvNTM4/MTMzNTEyL2VzL2Zv/dG8vc3RhaW5lZC1n/bGFzcy1pbi1zYWlu/dC1ob25vciVDMyVB/OS1kZXlsYXUtY2h1/cmNoLXRoZS1sYXN0/LXN1cHBlci5qcGc_/cz02MTJ4NjEyJnc9/MCZrPTIwJmM9UFhj/ZnlpZGo0alNMMWZy/Zkl5dFpwTnA4V05Y/b3k3TEFPQnBEMlht/VDlTaz0"
+            imageUrl: "https://res.cloudinary.com/dujwpfuml/image/upload/v1780817689/credo-apostoles_lxgfwn.png"
         },
         {
             id: "ogcrni-001", 
             title: "Credo Niceno",
             description: "Declaración de fe cristiana más antigua y ampliamente aceptada",
-            imageUrl: "https://imgs.search.brave.com/rpb1Jc0T9_fMcRCb_sVqrxELY0AkMpSEKL4o3AvV3B0/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi8zLzMxL05p/Y2FlYV9pY29uLmpw/Zy85NjBweC1OaWNh/ZWFfaWNvbi5qcGc"
+            imageUrl: "https://res.cloudinary.com/dujwpfuml/image/upload/v1780817462/credo-niceno_aaei4j.png"
         },
         {
             id: "ogmag-001", 
             title: "Magnificat",
             description: "Alabanza pronunciada por la Virgen María durante su visita a su prima Isabel",
-            imageUrl: "https://imgs.search.brave.com/_HJzad_zMEuZg_JtW_IcqsX41QD09EihM0UbooGK1Qk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly91cGxv/YWQud2lraW1lZGlh/Lm9yZy93aWtpcGVk/aWEvY29tbW9ucy90/aHVtYi9hL2E4L0Rv/bWVuaWNvX2doaXJs/YW5kYWlvJTJDX3Zp/c2l0YXppb25lJTJD/XzE0OTElMkNfZGFf/Y2FwcGVsbGFfZGlf/bG9yZW56b190b3Ju/YWJ1b25pX2luX3Mu/bS5fbWFkZGFsZW5h/X2RlJTI3X3Bhenpp/X2FfZmklMkNfMDIu/SlBHLzEyODBweC1E/b21lbmljb19naGly/bGFuZGFpbyUyQ192/aXNpdGF6aW9uZSUy/Q18xNDkxJTJDX2Rh/X2NhcHBlbGxhX2Rp/X2xvcmVuem9fdG9y/bmFidW9uaV9pbl9z/Lm0uX21hZGRhbGVu/YV9kZSUyN19wYXp6/aV9hX2ZpJTJDXzAy/LkpQRw"
+            imageUrl: "https://res.cloudinary.com/dujwpfuml/image/upload/v1780816963/magnificat_qtc8ha.png"
         },
         {
             id: "ovima-002", 
             title: "Consagración del hogar a la Virgen María",
             description: "Encomienda tu hogar y las personas que lo habitan a nuestra Madre",
-            imageUrl: "https://imgs.search.brave.com/5osM33q_YL3I6ZdFNu8YQ4CneyvxOf2EwN8ct_e1-uc/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9kZXNk/ZWxhZmUubXgvd3At/Y29udGVudC91cGxv/YWRzLzIwMjAvMTIv/dmlyZ2VuLWRlLWd1/YWRhbHVwZS5qcGVn"
+            imageUrl: "https://res.cloudinary.com/dujwpfuml/image/upload/v1780817461/virgen-guadalupe_rsee8q.png"
         },
         {
             id: "osmaca-002", 
